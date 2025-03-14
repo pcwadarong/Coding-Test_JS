ee77f7a03a0572f2aba38929d2d908ddbacd23ba
Chaen <cw06155@gmail.com>
2025-03-14 15:42:01 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.ts
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+
+const A = Number(input[0]);
+const B = Number(input[1]);
+
+const result = A > B ? '>' : A == B ? '==' : '<';
+console.log(result);
