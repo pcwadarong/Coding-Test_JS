2c2ff22d085fa1a6fcdc99bbf0c468ac369d1374
Chaen <cw06155@gmail.com>
2025-03-14 13:00:15 +0900

    [Bronze V] Title: 오늘 날짜, Time: 88 ms, Memory: 9300 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.ts
@@ -0,0 +1,4 @@
+const date = new Date();
+const convertedDate = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`
+
+console.log(convertedDate);
