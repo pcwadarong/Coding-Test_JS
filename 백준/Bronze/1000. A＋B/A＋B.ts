a8b9b5391a1af4102d1a52873bb8f188e41c5cfa
Chaen <cw06155@gmail.com>
2025-03-14 15:27:09 +0900

    [Bronze V] Title: A+B, Time: 100 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.ts
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+const num1 = Number(input[0]);
+const num2 = Number(input[1]);
+console.log(num1+num2);
