b06a46388cc4cdfaca1e1f83ebaa485043d9d474
Chaen <cw06155@gmail.com>
2025-03-15 20:36:44 +0900

    [Bronze III] Title: 알람 시계, Time: 100 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.ts
+++ 백준/Bronze/2884. 알람 시계/알람 시계.ts
@@ -3,9 +3,9 @@ let input = require('fs').readFileSync('/dev/stdin').toString().trim().split(' '
 const hour = input[0];
 const minutes = input[1];
 
-let finalHour = minutes >= 45 ? hour : hour - 1;
-let finalMinutes = minutes > 45 ? minutes - 45 : minutes === 45 ? 0 : minutes + 60 - 45;
+let finalHour = hour;
+let finalMinutes = minutes >= 45 ? minutes - 45 : minutes + 60 - 45;
 
-if (finalHour < 0) finalHour = finalHour + 24;
+if (minutes < 45) finalHour = hour === 0 ? 23 : hour - 1;
  
 console.log(`${finalHour} ${finalMinutes}`)
