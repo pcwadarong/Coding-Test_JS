aa1bd7753c773b76d128a406a7b8e9f1280f562c
Chaen <cw06155@gmail.com>
2025-03-16 04:23:47 +0900

    Update 최대공약수와 최소공배수.ts
    refactoring 소인수분해 방법

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.ts
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.ts
@@ -1,10 +1,8 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
-
 const [a, b] = input;
-const primeA = [1];
-const primeB = [1];
 
-const findPrimeFactors = (num:number, primes: Array<number>) => {
+const findPrimeFactors = (num:number): number[] => {
+    const primes: number[] = [1];
     for (let i = 2; i <= num; i++){
         while (num % i === 0){
             primes.push(i);
@@ -14,15 +12,14 @@ const findPrimeFactors = (num:number, primes: Array<number>) => {
     return primes;
 }
 
-const findLargestCommonDivisor = (a: number[], b: number[]) => {
-    const commonDivisors: number[] = [];
-
+const findLargestCommonDivisor = (a: number[], b: number[]): number => {
+    let gcd = 1;
     const tempA = [...a];
     const tempB = [...b];
 
     while (tempA.length > 0 && tempB.length > 0) {
         if (tempA[0] === tempB[0]) {
-            commonDivisors.push(tempA.shift()!);
+            gcd *= tempA.shift()!;
             tempB.shift();
         } else if (tempA[0] < tempB[0]) {
             tempA.shift();
@@ -31,36 +28,31 @@ const findLargestCommonDivisor = (a: number[], b: number[]) => {
         }
     }
 
-    return commonDivisors.reduce((acc, cur) => acc * cur, 1)
+    return gcd;
 }
 
 const findGreatestCommonMultiple = (a: number[], b: number[]) => {
-    const combinedDivisors: number[] = [];
-
+    let lcm = 1;
     const tempA = [...a];
     const tempB = [...b];
 
     while (tempA.length > 0 || tempB.length > 0){
-        if (tempA.length === 0) {
-            combinedDivisors.push(tempB.shift()!);
-        } else if (tempB.length === 0) {
-            combinedDivisors.push(tempA.shift()!);
-        } else if (tempA[0] === tempB[0]) {
-            combinedDivisors.push(tempA.shift()!);
+        if (tempA.length === 0 || (tempB.length > 0 && tempA[0] > tempB[0])) {
+            lcm*=tempB.shift()!
+        } else if (tempB.length === 0 || tempA[0] < tempB[0]) {
+            lcm*=tempA.shift()!
+        } else { //tempA[0] === tempB[0]
+            lcm*=tempA.shift()!
             tempB.shift();
-        } else if (tempA[0] < tempB[0]) {
-            combinedDivisors.push(tempA.shift()!);
-        } else {
-            combinedDivisors.push(tempB.shift()!);
         }
     }
-    return combinedDivisors.reduce((acc, cur) => acc * cur, 1)
+    return lcm;
 }
 
-const divisorA = findPrimeFactors(a, primeA);
-const divisorB = findPrimeFactors(b, primeB);
+const divisorA = findPrimeFactors(a);
+const divisorB = findPrimeFactors(b);
 const lcd = findLargestCommonDivisor(divisorA, divisorB);
 const gcm = findGreatestCommonMultiple(divisorA, divisorB);
 
-console.log(`${lcd}
-${gcm}`);
+console.log(lcd);
+console.log(gcm);
