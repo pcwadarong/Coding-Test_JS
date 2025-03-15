e39761cb1fb4a31c0aefa420622fdff71a3b6dd7
Chaen <cw06155@gmail.com>
2025-03-16 04:26:55 +0900

    Update 최대공약수와 최소공배수.ts
    유클리드 호제법 사용

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.ts
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.ts
@@ -1,58 +1,19 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
 const [a, b] = input;
 
-const findPrimeFactors = (num:number): number[] => {
-    const primes: number[] = [1];
-    for (let i = 2; i <= num; i++){
-        while (num % i === 0){
-            primes.push(i);
-            num = num/i;
-        }
+// 유클리드 호제법: 최대공약수 구하기, 값 교환(Swap) 방법 사용
+// a에 b를 넣고, b에 a%b를 넣어서 계속해서 수를 줄이다가 최종적으로 b = 0, a의 값이 gcd
+const findGcd = (a: number, b: number): number => {
+    while (b !== 0) {
+        [a, b] = [b, a % b];  
     }
-    return primes;
-}
+    return a;
+};
 
-const findLargestCommonDivisor = (a: number[], b: number[]): number => {
-    let gcd = 1;
-    const tempA = [...a];
-    const tempB = [...b];
+const gcd = findGcd(a, b);
 
-    while (tempA.length > 0 && tempB.length > 0) {
-        if (tempA[0] === tempB[0]) {
-            gcd *= tempA.shift()!;
-            tempB.shift();
-        } else if (tempA[0] < tempB[0]) {
-            tempA.shift();
-        } else {
-            tempB.shift();
-        }
-    }
+// 최대공배수란? 두 수의 곱을 가장 큰 약수로 나눈 값
+const lcm = (a * b) / gcd;
 
-    return gcd;
-}
-
-const findGreatestCommonMultiple = (a: number[], b: number[]) => {
-    let lcm = 1;
-    const tempA = [...a];
-    const tempB = [...b];
-
-    while (tempA.length > 0 || tempB.length > 0){
-        if (tempA.length === 0 || (tempB.length > 0 && tempA[0] > tempB[0])) {
-            lcm*=tempB.shift()!
-        } else if (tempB.length === 0 || tempA[0] < tempB[0]) {
-            lcm*=tempA.shift()!
-        } else { //tempA[0] === tempB[0]
-            lcm*=tempA.shift()!
-            tempB.shift();
-        }
-    }
-    return lcm;
-}
-
-const divisorA = findPrimeFactors(a);
-const divisorB = findPrimeFactors(b);
-const lcd = findLargestCommonDivisor(divisorA, divisorB);
-const gcm = findGreatestCommonMultiple(divisorA, divisorB);
-
-console.log(lcd);
-console.log(gcm);
+console.log(gcd);
+console.log(lcm);
