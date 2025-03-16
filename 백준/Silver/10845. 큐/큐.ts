1b03da145e8f7721ba0fe2d4b549fb21cd85b445
Chaen <cw06155@gmail.com>
2025-03-16 17:43:59 +0900

    Update 큐.ts
    if => switch

--- 백준/Silver/10845. 큐/큐.ts
+++ 백준/Silver/10845. 큐/큐.ts
@@ -5,35 +5,27 @@ const queue:number[] = [];
 const answerArr:number[] = [];
 
 for (let el of input){
-    if (el.startsWith('push')){
-        queue.push(el.split(' ')[1]);
-    } else if (el.startsWith('pop')){
-        if (queue[0]){
-            answerArr.push(queue[0]);
-            queue.shift();
-        } else {
-            answerArr.push(-1);
-        }
-    } else if (el.startsWith('size')){
-        answerArr.push(queue.length);
-    } else if (el.startsWith('empty')){
-        if (queue[0]){
-            answerArr.push(0);
-        } else {
-            answerArr.push(1);
-        }
-    } else if (el.startsWith('front')){
-        if (queue[0]){
-            answerArr.push(queue[0]);
-        } else {
-            answerArr.push(-1);
-        }
-    } else if (el.startsWith('back')){
-        if (queue[0]){
-            answerArr.push(queue[queue.length-1]);
-        } else {
-            answerArr.push(-1);
-        }
+    switch (el.split(' ')[0]){
+        case 'push':
+            queue.push(el.split(' ')[1]);
+            break;
+        case 'pop':
+            answerArr.push(queue[0] ? queue.shift()! : -1);
+            break;
+        case 'size':
+            answerArr.push(queue.length);
+            break;
+        case 'empty':
+            answerArr.push(queue[0] ? 0 : 1);
+            break;
+        case 'front':
+            answerArr.push(queue[0] ? queue[0] : -1);
+            break;
+        case 'back':
+            answerArr.push(queue[0] ? queue[queue.length-1] : -1);
+            break;
+        default:
+            break;
     }
 }
 console.log(answerArr.join('\n'))
