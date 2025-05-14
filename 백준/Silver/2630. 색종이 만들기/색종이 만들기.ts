c02a8e483b3e741e19a64fb7788ac2fe6777ecc9
Chaen <cw06155@gmail.com>
2025-05-14 14:59:43 +0900

    [Silver II] Title: 색종이 만들기, Time: 156 ms, Memory: 10232 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.ts
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.ts
@@ -1,42 +1,32 @@
 const [N, ...lines] = require('fs').readFileSync("/dev/stdin").toString().trim().split('\n');
 const n = +N;
-const grid = Array.from({ length: n }, () => Array(n).fill(0));
-let white = 0;
-let blue = 0;
-
-for (let i = 0; i < n; i++){
-    for (let j = 0; j < n; j++){
-        const line = lines[i].trim().split(' ').map(Number);
-        grid[i][j] = line[j];
-    }
-}
-
-function isSame(x, y, size){
-    const color = grid[y][x];
-   for (let i = y; i < y + size; i++) {
-        for (let j = x; j < x + size; j++) {
-            if (grid[i][j] !== color) return false;
-        }
-    }
-
-    return true;
+const grid = lines.map(line => line.split(' ').map(Number))
+const colorCount = {
+    white: 0,
+    blue: 0
 }
 
 function divide(x, y, size){
-    if (isSame(x, y, size)){
-        const color = grid[y][x];
-        color === 0 ? white++ : blue++;
-        return;
+    const color = grid[y][x];
+    let isSame = true;
+
+    for (let i = y; i < y + size; i++) {
+        for (let j = x; j < x + size; j++) {
+            if (grid[i][j] !== color) isSame = false;
+        }
     }
 
-    const half = size / 2;
-    divide(x, y, half);
-    divide(x + half, y, half);
-    divide(x, y + half, half);
-    divide(x + half, y + half, half);
+    if (isSame) color === 0 ? colorCount.white++ : colorCount.blue++;
+    else {
+        const half = size / 2;
+        divide(x, y, half);
+        divide(x + half, y, half);
+        divide(x, y + half, half);
+        divide(x + half, y + half, half);
+    }
 }
 
 divide(0, 0, n);
 
-console.log(white);
-console.log(blue);
+console.log(colorCount.white);
+console.log(colorCount.blue);
