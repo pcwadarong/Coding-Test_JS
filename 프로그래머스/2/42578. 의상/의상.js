0b1ec681b2e813c3f8bc87882de72f8dbd642993
Chaen <cw06155@gmail.com>
2025-06-17 04:56:36 +0900

    [level 2] Title: 의상, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,13 +1,14 @@
 function solution(clothes) {
     let answer = 1;
     let comb = new Map();
-    clothes.forEach((cloth)=>{
-        comb.set(cloth[1], (comb.get(cloth[1])||0) + 1);
-    })
 
-    comb.forEach (count => {
+    clothes.forEach(([_, type]) => {
+        comb.set(type, (comb.get(type) || 0) + 1);
+    });
+
+    comb.forEach(count => {
         answer *= (count + 1);
-    })
-    
+    });
+
     return answer - 1;
 }
