45120f05c7804403ad6108cbae5cd96547560cd7
Chaen <cw06155@gmail.com>
2025-06-20 10:53:50 +0900

    Update 신규 아이디 추천.js

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,17 +1,12 @@
 function solution(new_id) {
-    let answer = new_id.toLowerCase()
-      .replace(/[^a-z\d\-_.]/g, '')
-      .replace(/\.{2,}/g, '.');
+  let answer = new_id
+    .toLowerCase()
+    .replace(/[^a-z\d\-_.]/g, "")
+    .replace(/\.{2,}/g, ".")
+    .replace(/^\.|\.$/g, "");
 
-    if (answer.startsWith('.')) answer = answer.substr(1);
-    if (answer.endsWith('.')) answer = answer.slice(0, -1);
-    
-    if (!answer) answer = 'a';
-    
-    if (answer.length >= 16) answer = answer.slice(0, 15);
-    if (answer.endsWith('.')) answer = answer.slice(0, -1);
-    
-    answer = answer.padEnd(3, answer[answer.length-1]);
-    
-    return answer;
+  if (!answer) answer = "a";
+
+  answer = answer.slice(0, 15).replace(/\.$/, "");
+  return answer.padEnd(3, answer.at(-1));
 }
