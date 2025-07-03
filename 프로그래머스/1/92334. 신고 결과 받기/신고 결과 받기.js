2b4bf36026293ac6da61040e4953ea5cb352a662
Chaen <cw06155@gmail.com>
2025-07-04 04:08:22 +0900

    Update 신고 결과 받기.js

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,20 +1,21 @@
 function solution(id_list, report, k) {
-    const records = new Map(id_list.map(e => [e, new Set()]));
-    const reportedBy = new Map(id_list.map(e => [e, new Set()]));
+    // set으로 중복 신고 삭제
+    let reports = [...new Set(report)].map(e => e.split(' '));
+    const count = new Map(id_list.map(e => [e, 0]));
     const answer = new Map(id_list.map(e => [e, 0]));
-    
-    for (let record of report){
-        const [reporterId, reportedId] = record.split(' ');
-        records.get(reporterId).add(reportedId);
-        reportedBy.get(reportedId).add(reporterId);
+
+    // id당 받은 신고 수 저장
+    for (let record of reports){
+        count.set(record[1], count.get(record[1]) + 1);
     }
-    
-    const reportedMember = [...reportedBy].filter(([key, value]) => value.size >= k).map(([key]) => key);
-    for (const [key, value] of records){
-        for (let reportedId of reportedMember){
-            if ([...value].includes(reportedId)) answer.set(key, (answer.get(key) ?? 0) + 1);
+
+    // reportedId의 신고 수가 k 이상이면 reporterId의 숫자 추가(메일 받을 사람 수)
+    for (let record of reports){
+        if (count.get(record[1]) >= k){
+            answer.set(record[0], answer.get(record[0]) + 1);
         }
     }
-    
+
+    // reporterId의 value 반환
     return [...answer].map(e => e[1]);
 }
