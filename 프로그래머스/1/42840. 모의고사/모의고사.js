18f086a1899ce8c6adc5bd04679ab989a5afab06
Chaen <cw06155@gmail.com>
2026-02-25 23:08:35 +0900

    [level 1] Title: 모의고사, Time: 32.58 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -0,0 +1,15 @@
+function solution(answers) {
+    const scores = [0, 0, 0];
+    const p1 = [1, 2, 3, 4, 5];
+    const p2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    const p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    
+    for (let i = 0; i < answers.length; i++){
+        if (answers[i] === p1[i % p1.length]) scores[0]++;
+        if (answers[i] === p2[i % p2.length]) scores[1]++;
+        if (answers[i] === p3[i % p3.length]) scores[2]++;
+    }
+    
+    const max = Math.max(...scores);
+    return scores.map((v, i) => v === max ? i + 1 : null).filter(v => v !== null);
+}
